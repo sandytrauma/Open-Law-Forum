77d5c843e7de9db1b05223e2225325aa8acf83b9
sandytrauma <sksandeep443gmail.com>
2024-11-22 15:43:33 +0530

    3 and 1

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,6 @@ const nextConfig = {
     unoptimized: true, 
   }, 
 
- 
-    output: 'export',
-
-    experimental: {
-      appDir: true, // Enable the App Router
-    },
-
 };
 
 export default nextConfig;
