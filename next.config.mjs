47ed01448cab57a9cb54f5a970ac35b0602248ea
sandytrauma <sksandeep443gmail.com>
2024-11-22 12:41:34 +0530

    toml file corrected

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     unoptimized: true, 
   }, 
+
+    // If using Netlify, ensure this setting aligns with its plugin's requirements.
+    distDir: '.next', // Default; should not be changed unless necessary.
+
 };
 
 export default nextConfig;
