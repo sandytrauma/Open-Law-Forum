fde8dcf44563a8653c9569b4465d3662df676234
sandytrauma <sksandeep443@gmail.com>
2024-12-28 16:05:56 +0530

    api reset again

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, 
-  }, 
-
-  output:"export",
+    unoptimized: true,  // Disable Next.js image optimization
+  },
+  reactStrictMode: true,  // Enable React Strict Mode
+  // Remove 'output: "export"' if using server-side rendering or serverless functions
 };
 
 export default nextConfig;
