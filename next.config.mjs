3d4f07d60df56105493dd7001a7da45c6d66d44e
sandytrauma <sksandeep443@gmail.com>
2024-12-30 14:15:12 +0530

    api route updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const nextConfig = {
   },
   reactStrictMode: true,  // Enable React Strict Mode
  output: "export",
+
+ async rewrites() {
+  return [
+    {
+      source: '/api/:path*',
+      destination: 'https://open-law-forum.netlify.app/:path*', // Replace with the actual API URL
+    },
+  ];
+},
 };
 
 export default nextConfig;
