dfe248fb8899cbb982877454c298c16f03e9348f
sandytrauma <sksandeep443@gmail.com>
2024-12-31 13:50:43 +0530

    path adjusted for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,17 @@ const nextConfig = {
     unoptimized: true,  // Disable Next.js image optimization
   },
   reactStrictMode: true,  // Enable React Strict Mode
- output: "export",
+
 
  async rewrites() {
   return [
     {
       source: '/api/:path*',
       destination: 'https://open-law-forum.netlify.app/:path*', // Replace with the actual API URL
+    },
+    {
+      source: '/api/askai',
+      destination: '/.netlify/functions/askai', // Adjust to your function URL if needed
     },
   ];
 },
